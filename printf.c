16f1cd80d8b600df713d475b2c5c4f7734f88cb2
MenacingManatee <brett.p.davis101@gmail.com>
2020-03-14 00:32:59 +0000

    Adds base printf function and dependent functions Adds _calloc from previous project to be used in other functions Adds _strlen from previous project for usage in other functions Adds Calloced buffer to printf that is written at the end

---
+++ printf.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+#include <stdio.h>
+
+void _strcpy(char *dest, char *src);
+/**
+ * _printf - prints a formatted buffer of vriable arguments and types
+ * @format: String literal containing % before format specifier
+ *
+ * Return: Number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i, j;
+	char *buff = _calloc(sizeof(char) * 1024, sizeof(char));
+	/*char *tmp; --var not in use yet*/
+	va_list args;
+	char *test = "placeholder";
+
+	i = j = 0;
+	va_start(args, format);
+	for (i = 0, j = 0; format[i]; i++, j++)
+	{
+		if (format[i] != '%')
+		{
+			buff[j] = format[i];
+		}
+		else
+		{
+			/*char *tmp = get_type(*(format + i));*/
+			_strcpy(buff + j, test);
+			j += _strlen(test);
+			i++; /*replace later with dynamic check*/
+		}
+	}
+	buff[j] = '\0';
+	write('\0', buff, j);
+	return (j);
+}
+/**
+ * _strcpy - copies src string to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: void
+ */
+void _strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i]; i++)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+
+	}
+}
