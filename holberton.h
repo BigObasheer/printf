42f804a103fbd640d80c5d946853a77081d45be9
BigObasheer <838@holbertonschool.com>
2020-03-16 00:54:29 +0000

    Add get_type function prototype

--- holberton.h
+++ holberton.h
@@ -1,5 +1,6 @@
 #ifndef H0LBERTON_H
 #define HOLBERTON_H
+
 int _putchar(char c);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -7,4 +8,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 void print_all(const char * const format, ...);
 
 int _strlen(char *s);
+
+int get_format(const char *format, va_list args);
+
 #endif
